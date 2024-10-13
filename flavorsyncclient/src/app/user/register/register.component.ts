aecb78d9756e8b0d7361aa8dc1024a24682a8056
mittalgmt <gamitmittal0817@outlook.com>
2024-10-13 11:41:15 +0530

    pipes

--- flavorsyncclient/src/app/user/register/register.component.ts
+++ flavorsyncclient/src/app/user/register/register.component.ts
@@ -1,25 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { combineLatest } from 'rxjs';
+import { FirstKeyPipe } from "../../shared/pipes/first-key.pipe";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule, FirstKeyPipe],
   templateUrl: './register.component.html',
   styles: ``
 })
 export class RegisterComponent {
+
   constructor(public formBuilder: FormBuilder){}
+  isSubmitted:boolean = false;
+
+  passwordMatchValidator: ValidatorFn = (control:AbstractControl) : null =>
+  {
+    const password = control.get('password');
+    const confirempassword = control.get('confirmpassword')
+
+    if(password && confirempassword && password.value != confirempassword.value)
+      confirempassword?.setErrors({passwordMismatch:true})
+    else
+    confirempassword?.setErrors(null)
+    return null;
+  }
+
 
   form = this.formBuilder.group({
-    fullName : [''],
-    email : [''],
-    password : [''],
+    fullName : ['',Validators.required],
+    email : ['',[Validators.required,Validators.email]],
+    password : ['',[
+      Validators.required,
+      Validators.minLength(6),
+      Validators.pattern(/(?=.*[^a-zA-Z0-9])/)]],
     confirempassword : [''],
     
-  })
+  },{validators:this.passwordMatchValidator})
+
+ 
 
   onSubmit(){
+    this.isSubmitted = true;
     console.log(this.form);
+  }
+
+  hasDisplayableError(controlName: string):Boolean{
+     const control = this.form.get(controlName);
+     return Boolean(control?.invalid) &&
+     (this.isSubmitted || Boolean(control?.touched)|| Boolean(control?.dirty))
   }
 }
